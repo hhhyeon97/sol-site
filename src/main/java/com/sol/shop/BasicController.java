f607a4874eaa5a25df904ce0b0a5861143791b93
minsol <kth083030@naver.com>
2024-04-29 23:50:24 +0900

    Controller test

---
+++ src/main/java/com/sol/shop/BasicController.java
@@ -0,0 +1,33 @@
+package com.sol.shop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.ZonedDateTime;
+
+@Controller
+public class BasicController {
+   @GetMapping("/")
+    String hello(){
+    return "index.html";
+   }
+
+    @GetMapping("/login")
+    @ResponseBody
+    String login(){
+        return "로그인입니다 하이";
+    }
+
+    @GetMapping("/mypage")
+    @ResponseBody
+    String myPage(){
+        return "마이페이지입니다 하이";
+    }
+
+    @GetMapping("/date")
+    @ResponseBody
+    String date(){
+       return ZonedDateTime.now().toString();
+    }
+}
--- src/main/java/com/sol/shop/ShopApplication.java
+++ src/main/java/com/sol/shop/ShopApplication.java
@@ -5,44 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ShopApplication {
-String hello() {
-	return "hi!";
-}
+
 	public static void main(String[] args) {
 		SpringApplication.run(ShopApplication.class, args);
 
-		String hi = "hello!";
-		int age = 20;
-		final int count = 1;
-//		count = 2;
-//		System.out.println(hi);
-	/*	if(age==20){
-			System.out.println(count);
-		}
-		for(int i=0; i<3; i++){
-			System.out.println(hi);
-		}*/
-		Test test = new Test();
-		System.out.println(test.name);
-		test.hello();
-		Friend friend = new Friend("Park",15);
-		System.out.println(friend.name);
-		System.out.println(friend.age);
 	}
 
 }
 
-class Test {
-	String name = "kim";
-	void hello(){
-		System.out.println("안녕");
-	}
-}
-class Friend {
-	String name = "Lee";
-	int age = 20;
-	Friend(String name, int age){
-		this.name = name;
-		this.age = age;
-	}
-}
