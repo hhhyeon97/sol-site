90ccdd12ddebccb98dc0864486ceaffe57f1e429
minsol <kth083030@naver.com>
2024-04-30 23:22:57 +0900

    mysql연결

---
+++ src/main/java/com/sol/shop/Item.java
@@ -0,0 +1,14 @@
+package com.sol.shop;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Item {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    public String title;
+    public Integer price;
+
+
+}
---
+++ src/main/java/com/sol/shop/Notice.java
@@ -0,0 +1,17 @@
+package com.sol.shop;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+@Entity
+public class Notice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private Date date;
+}
