382b395d258a5ce6ff261761643b90eb2ac055cd
minsol <kth083030@naver.com>
2024-05-02 19:54:58 +0900

    서비스분리

--- src/main/java/com/sol/shop/ItemController.java
+++ src/main/java/com/sol/shop/ItemController.java
@@ -17,17 +17,12 @@ import java.util.Optional;
 public class ItemController {
 
     private final ItemRepository itemRepository;
+    private final ItemService itemService;
 
     @GetMapping("/list")
     String list(Model model){
         List<Item> result = itemRepository.findAll(); //리스트 자료형으로 가져옴
         model.addAttribute("items",result);
-
-        String firstTitle = null;
-        if (!result.isEmpty()) {
-            firstTitle = result.get(0).getTitle();
-        }
-        System.out.println("첫 번째 제목: " + firstTitle);
 
         return "list.html";
     }
@@ -39,24 +34,20 @@ public class ItemController {
 
     @PostMapping("/add")
     String addPost(@RequestParam String title, @RequestParam Integer price) {
-        Item item = new Item();
-        item.setTitle(title);
-        item.setPrice(price);
-        itemRepository.save(item);
+        itemService.saveItem(title, price);
         return "redirect:/list";
     }
 
     @GetMapping("/detail/{id}")
-    String detail(@PathVariable Long id, Model model) throws Exception{
-        throw new Exception();
-//            Optional<Item> result = itemRepository.findById(id);
-//            if(result.isPresent()){
-//                Item item = result.get();
-//                model.addAttribute("item",item);
-//                return "detail.html";
-//            }else {
-//                return "404page.html";
-//            }
+    String detail(@PathVariable Long id, Model model){
+            Optional<Item> result = itemRepository.findById(id);
+            if(result.isPresent()){
+                Item item = result.get();
+                model.addAttribute("item",item);
+                return "detail.html";
+            }else {
+                return "404page.html";
+            }
     }
 
 }
