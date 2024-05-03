fe7c33bce3578d0f1466926d8975ec55b46988fd
minsol <kth083030@naver.com>
2024-05-03 23:16:12 +0900

    회원가입테스트

---
+++ src/main/java/com/sol/shop/member/MemberController.java
@@ -0,0 +1,32 @@
+package com.sol.shop.member;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/join")
+    String join(){
+        return "join.html";
+    }
+
+    @PostMapping("/member")
+    String addMember(@RequestParam String username,String password, String displayName){
+        Member member = new Member();
+        member.setUsername(username);
+        var hash = new BCryptPasswordEncoder().encode(password);
+        member.setPassword(hash);
+        member.setDisplayName(displayName);
+        memberRepository.save(member);
+        return "redirect:/list";
+    }
+
+}
