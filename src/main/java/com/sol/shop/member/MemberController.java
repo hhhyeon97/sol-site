7023c05370b8ba24bcc969e3a2ad23b0279ff4e3
minsol <kth083030@naver.com>
2024-05-03 23:28:08 +0900

    해싱빈등록

--- src/main/java/com/sol/shop/member/MemberController.java
+++ src/main/java/com/sol/shop/member/MemberController.java
@@ -2,6 +2,7 @@ package com.sol.shop.member;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class MemberController {
 
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @GetMapping("/join")
     String join(){
@@ -22,7 +24,7 @@ public class MemberController {
     String addMember(@RequestParam String username,String password, String displayName){
         Member member = new Member();
         member.setUsername(username);
-        var hash = new BCryptPasswordEncoder().encode(password);
+        var hash = passwordEncoder.encode(password);
         member.setPassword(hash);
         member.setDisplayName(displayName);
         memberRepository.save(member);
