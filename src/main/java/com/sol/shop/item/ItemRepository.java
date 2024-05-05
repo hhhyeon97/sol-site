0cf309bfeefe404b3d30de80bad9b09e8f917f07
minsol <kth083030@naver.com>
2024-05-05 23:03:25 +0900

    검색결과fulltextindex테스트

--- src/main/java/com/sol/shop/item/ItemRepository.java
+++ src/main/java/com/sol/shop/item/ItemRepository.java
@@ -3,6 +3,7 @@ package com.sol.shop.item;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -11,4 +12,9 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
 //    Page<Item> findPageBy(Pageable page);
     Page<Item> findAll(Pageable pageable);
     List<Item> findAllByTitleContains(String title);
+
+//    @Query(value = "select * from item where id = ?2", nativeQuery = true)
+    @Query(value = "select * from item where match(title) against(?1)", nativeQuery = true)
+    List<Item> rawQuery1(String text);
+
 }
