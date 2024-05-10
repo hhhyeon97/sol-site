4a157ac5832801a0a91e30d43fc66b8109c268d7
minsol <kth083030@naver.com>
2024-05-10 21:21:02 +0900

    회원가입수정

--- src/main/java/com/sol/shop/member/MemberController.java
+++ src/main/java/com/sol/shop/member/MemberController.java
@@ -6,6 +6,8 @@ import com.sol.shop.sales.Sales;
 import com.sol.shop.sales.SalesRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -55,6 +57,20 @@ public class MemberController {
             return "join.html";
         }
 
+    }
+    @GetMapping("/checkUsername")
+    @ResponseBody
+    public ResponseEntity<String> checkUsername(@RequestParam String username) {
+        // 아이디가 이미 존재하는지 확인하는 로직
+        boolean isUsernameExists = memberRepository.existsByUsername(username);
+
+        if (isUsernameExists) {
+            // 이미 존재하는 아이디인 경우
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Username already exists");
+        } else {
+            // 존재하지 않는 아이디인 경우
+            return ResponseEntity.ok("Username is available");
+        }
     }
 
 
